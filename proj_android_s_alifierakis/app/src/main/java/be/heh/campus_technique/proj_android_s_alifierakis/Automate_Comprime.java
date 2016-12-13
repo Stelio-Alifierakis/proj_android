73315a1772bb9e402449f00c46359e4550750c2f
DESKTOP-R9B14N3\steli <stelio_a@hotmail.fr>
2016-12-13 12:28:22 +0100

    layouts

---
+++ proj_android_s_alifierakis/app/src/main/java/be/heh/campus_technique/proj_android_s_alifierakis/Automate_Comprime.java
@@ -0,0 +1,14 @@
+package be.heh.campus_technique.proj_android_s_alifierakis;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Automate_Comprime extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_automate__comprime);
+    }
+}
